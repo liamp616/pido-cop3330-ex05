ac4792c154a17e25fe62094f34893accf62469de
Liam <xlime1p@gmail.com>
2021-06-02 23:37:22 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,64 @@
+package base;
+
+import java.util.Scanner;
+
+/*
+Exercise 5 - Simple Math
+You’ll often write programs that deal with numbers. And depending on the programming language you use, you’ll have to convert the inputs
+you get to numerical data types.
+
+Write a program that prompts for two numbers. Print the sum, difference, product, and quotient of those numbers as shown in the example output:
+
+Example Output
+
+What is the first number? 10
+What is the second number? 5
+10 + 5 = 15
+10 - 5 = 5
+10 * 5 = 50
+10 / 5 = 2
+
+Constraints
+
+Values coming from users will be strings. Ensure that you convert these values to numbers before doing the math.
+Keep the inputs and outputs separate from the numerical conversions and other processing.
+Generate a single output statement with line breaks in the appropriate spots.
+
+Challenges
+
+Revise the program to ensure that inputs are entered as numeric values. Don’t allow the user to proceed if the value entered is not numeric.
+Don’t allow the user to enter a negative number.
+Break the program into functions that do the computations.
+Implement this program as a GUI program that automatically updates the values when any value changes.
+ */
+
+public class App {
+    Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+
+        int firstNum = myApp.readFirstNum();
+        int secNum = myApp.readSecNum();
+        myApp.math(firstNum, secNum);
+    }
+
+    public void math(int firstNum, int secNum) {
+        System.out.println(firstNum + " + " + secNum + " = " + (firstNum + secNum));
+        System.out.println(firstNum + " - " + secNum + " = " + (firstNum - secNum));
+        System.out.println(firstNum + " * " + secNum + " = " + (firstNum * secNum));
+        System.out.println(firstNum + " / " + secNum + " = " + (firstNum / secNum));
+    }
+
+    public int readSecNum() {
+        System.out.print("What is the second number? ");
+        int secNum = in.nextInt();
+        return secNum;
+    }
+
+    public int readFirstNum() {
+        System.out.print("What is the first number? ");
+        int firstNum = in.nextInt();
+        return firstNum;
+    }
+}
